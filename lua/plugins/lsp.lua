8612d2fc589b007b7b1b0e941a915ac548e41911
Angellovem <aeduardom27@gmail.com>
2025-04-15 12:12:11 -0500

    Fixed the lazyvim autoinstallation and removed some lsp language servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -207,7 +207,6 @@ return {
             --    https://github.com/pmizio/typescript-tools.nvim
             --
             -- But for many setups, the LSP (`ts_ls`) will work just fine
-            ts_ls = {},
             ruff = {},
             pylsp = {
                 settings = {
@@ -225,11 +224,7 @@ return {
                     },
                 },
             },
-            html = { filetypes = { 'html', 'twig', 'hbs' } },
-            cssls = {},
-            sqlls = {},
             gopls = {},
-            clangd = {},
             lua_ls = {
                 -- cmd = {...},
                 -- filetypes = { ...},
@@ -269,11 +264,6 @@ return {
         --
         -- You can add other tools here that you want Mason to install
         -- for you, so that they are available from within Neovim.
-        local ensure_installed = vim.tbl_keys(servers or {})
-        vim.list_extend(ensure_installed, {
-            'stylua', -- Used to format Lua code
-        })
-        require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
         require('mason-lspconfig').setup {
             ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
